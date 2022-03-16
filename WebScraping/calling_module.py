283a3b50de221f5a73728e26486bf6ab9f55799d
Harsha vardhan <harshamadisetty@gmail.com>
2022-03-16 13:14:37 +0530

    adding calculator files

---
+++ WebScraping/calling_module.py
@@ -0,0 +1,18 @@
+import Calculator as c
+x = 7
+y = 9
+
+addition = c.add(x, y)
+print(addition)
+
+substraction = c.sub(x, y)
+print(substraction)
+
+multiplication = c.mul(x, y)
+print(multiplication)
+
+division = c.div(x, y)
+print(division)
+
+print(dir(cal))
+print(cal.path)
